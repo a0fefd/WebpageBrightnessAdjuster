293006af9402c0fcd7a5d48b81fd6aaaf8f856f1
a0fefd <52446438+a0fefd@users.noreply.github.com>
2024-10-22 09:27:05 +1000

    Fixed webpage loading with an initial "flashbang"

--- content.js
+++ content.js
@@ -1,5 +1,9 @@
-setInterval(function () {
+function getSetBrightness() {
     chrome.runtime.sendMessage({ action: 'get_data_content' }, function (response) {
         document.querySelector("html").style.filter = "brightness("+response.data[0]+"%)";
     });
-}, 500)
+    return "100";
+}
+getSetBrightness();
+setInterval(getSetBrightness, 500)
+
